a4c129d1e779381fbc5cdc9925e4aef65fe51a3a
Kaihong Huang <kaihong.huang11@gmail.com>
2016-12-11 16:16:28 +0100

    implemented IterationCallback

--- examples/quadratic/quadratic.py
+++ examples/quadratic/quadratic.py
@@ -3,7 +3,12 @@
 from cyres import *
 from cost_functions.wrappers import SimpleCostFunction
 
-x = np.array([5.])
+iter_cnt = [0]
+def foo():
+    iter_cnt[0] += 1
+    print iter_cnt[0]
+
+x = np.array([105.])
 problem = Problem()
 problem.add_residual_block(SimpleCostFunction(), SquaredLoss(), [x])
 
@@ -13,7 +18,7 @@ options.linear_solver_type = LinearSolverType.DENSE_QR
 options.trust_region_strategy_type = TrustRegionStrategyType.DOGLEG
 options.dogleg_type = DoglegType.SUBSPACE_DOGLEG
 options.minimizer_progress_to_stdout = False
-
+options.add_callback(SimpleCallback(foo))
 summary = Summary()
 
 solve(options, problem, summary)
